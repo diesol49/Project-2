9e23d97338262f8177202cf67c4fa77a50e16c3d
gravitation <chisong@ucdavis.edu>
2019-06-28 23:28:21 -0700

    login validation added

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -10,8 +10,9 @@ module.exports = function(app) {
       });
     });
   });
-
-  // login page
+//'GET' routes
+// ---------------------------------------------------------
+  // Login page
   app.get("/login", function(req, res) {
     db.User.findOne({ where: {} }).then(function(result) {
       res.render("login", {
@@ -25,17 +26,58 @@ module.exports = function(app) {
   app.get("/register/", function(req, res) {
     db.User.findOne({}).then(function(result) {
       res.render("register", {
-        msg: "register your",
+        msg: "good to see you",
         example: result
       });
     });
+  });
+
+  // Dashboard page
+  app.get("/dashboard/", function(req, res) {
+    res.render("dashboard");
   });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
   });
+  // ------------------------------------------------------
+  //'Post' routes
+  // Register handle
+  app.post("/register", function(req, res) {
+    var { userName, userEmail, userPassword, userPassword2 } = req.body;
+    var errors = [];
+
+    // Check required fields
+    if(!userName || !userEmail || !userPassword || !userPassword2) {
+      errors.push({ msg: "Please fill in all fields"});
+    }
+
+    // Check passwords match
+    if(userPassword !== userPassword2) {
+      errors.push({ msg: "Passwords do not match"});
+    }
+
+    // Check password length
+    if (userPassword.length < 6) {
+      errors.push({ meg: "Password should be at least 6 charcters"});
+    }
+
+    if (errors.length > 0) {
+      res.render("register", {
+        errors,
+        userName,
+        userEmail,
+        userPassword,
+        userPassword2
+      });
+    } else {
+      res.end("pass");
+    }
+  });
 };
+
+
 
 // app.get("/login/:id", function(req, res) {
 //   db.User.findOne({ where: { id: req.params.id } }).then(function(result) {
