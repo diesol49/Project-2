254ff788fa23e1c690f9115897304e833419d286
gravitation <chisong@ucdavis.edu>
2019-06-29 00:03:59 -0700

    functioning registration with validation

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,5 @@
 var db = require("../models");
+var bcrypt = require("bcryptjs");
 
 module.exports = function(app) {
   // Routes
@@ -10,8 +11,8 @@ module.exports = function(app) {
       });
     });
   });
-//'GET' routes
-// ---------------------------------------------------------
+  //'GET' routes
+  // ---------------------------------------------------------
   // Login page
   app.get("/login", function(req, res) {
     db.User.findOne({ where: {} }).then(function(result) {
@@ -42,6 +43,7 @@ module.exports = function(app) {
     res.render("404");
   });
   // ------------------------------------------------------
+
   //'Post' routes
   // Register handle
   app.post("/register", function(req, res) {
@@ -49,18 +51,21 @@ module.exports = function(app) {
     var errors = [];
 
     // Check required fields
-    if(!userName || !userEmail || !userPassword || !userPassword2) {
-      errors.push({ msg: "Please fill in all fields"});
+    if (!userName || !userEmail || !userPassword || !userPassword2) {
+      errors.push({ msg: "Please fill in all fields" });
+      console.log(errors[0]);
     }
 
     // Check passwords match
-    if(userPassword !== userPassword2) {
-      errors.push({ msg: "Passwords do not match"});
+    if (userPassword !== userPassword2) {
+      errors.push({ msg: "Passwords do not match" });
+      console.log(errors[0]);
     }
 
     // Check password length
     if (userPassword.length < 6) {
-      errors.push({ meg: "Password should be at least 6 charcters"});
+      errors.push({ meg: "Password should be at least 6 charcters" });
+      console.log(errors[0]);
     }
 
     if (errors.length > 0) {
@@ -72,12 +77,44 @@ module.exports = function(app) {
         userPassword2
       });
     } else {
-      res.end("pass");
+      // Validation passed
+      db.User.findOne({ userEmail: userEmail }).then(function(User) {
+        if (User) {
+          // User exists
+          errors.push({ msg: "Email is already registered" });
+          res.render("register", {
+            errors,
+            userName,
+            userEmail,
+            userPassword,
+            userPassword2
+          });
+        } else {
+          const newUser = new User({
+            userName,
+            userEmail,
+            userPassword
+          });
+
+          // Hash Password
+          bcrypt.genSalt(10, function(err, salt) {
+            bcrypt.hash(newUser.userPassword, salt, function(err, hash) {
+              if(err) throw err;
+              // Set password to hashed
+              newUser.userPassword = hash;
+              // Save user
+              newUser.save()
+              .then(user => {
+                res.redirect('/login');
+              })
+              .catch(err => console.log(err));
+            });
+          });
+        }
+      });
     }
   });
 };
-
-
 
 // app.get("/login/:id", function(req, res) {
 //   db.User.findOne({ where: { id: req.params.id } }).then(function(result) {
