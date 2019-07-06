a3417308e83a44dca78353ab39cc45fc74455088
gravitation <chisong@ucdavis.edu>
2019-07-05 21:33:47 -0700

    added foreign keys to data

--- models/workExp.js
+++ models/workExp.js
@@ -18,5 +18,15 @@ module.exports = function(sequelize, Sequelize) {
       type: Sequelize.STRING
     }
   });
+
+  WorkExp.associate = function(models) {
+    // We're saying that a Post should belong to an Author
+    // A Post can't be created without an Author due to the foreign key constraint
+    WorkExp.belongsTo(models.Version, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
   return WorkExp;
 };
